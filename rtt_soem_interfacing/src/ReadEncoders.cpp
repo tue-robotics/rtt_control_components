a6ba85e1b0a02b368806ee7ecbee0a92ddc49652
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2011-11-01 20:51:02 +0000

    Encoders
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/rtt_generic_components@5909 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ rtt_soem_interfacing/src/ReadEncoders.cpp
@@ -0,0 +1,111 @@
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <ocl/Component.hpp>
+#include <soem_beckhoff_drivers/EncoderMsg.h>
+
+#include "ReadEncoders.hpp"
+
+
+using namespace RTT;
+using namespace soem_beckhoff_drivers;
+using namespace AMIGO;
+
+ReadEncoders::ReadEncoders(const string& name) : TaskContext(name, PreOperational)
+{
+  // Creating Properties
+  addProperty( "enc2SI", enc2SI ).doc("Value to convert the encoder value to an SI value. Typically 2pi/(encodersteps_per_rev*gearbox)");
+  addProperty( "encoderbits", encoderbits ).doc("Saturation value of the encoder. For example: 65536 for a 16 bit encoder");
+  //const uint encoderbits = 65536;
+}
+ReadEncoders::~ReadEncoders(){}
+
+bool ReadEncoders::configureHook()
+{
+  // Determine number of encoders to be read
+  N = enc2SI.size();
+  SI_value.resize(N);
+  init_SI_value.resize(N);
+
+  if (N > maxN)
+  {
+    log(Error)<<"You're trying to read "<<N<<" encoders while a maximum of "<<maxN<<" is hardcoded. Appologies"<<endlog();
+    return false;
+  }
+  else if (N == 0)
+  {
+    log(Warning)<<"No conversion factors given (enc2SI), make sure you're loading the properties ok"<<endlog();
+    return false;
+  }
+  log(Info)<<"Creating ports for "<<N<<" encoders."<<endlog();
+
+
+  // Creating ports
+  for ( uint i = 0; i < N; i++ )
+  {
+    string name_inport = "enc"+to_string(i+1)+"_in";
+    addPort( name_inport, inport_enc[i] );
+  }
+  addPort( "out", outport );
+  SI_value.resize(4);
+
+  counter = 0;
+
+  return true;
+}
+
+bool ReadEncoders::startHook()
+{
+  // Check validity of Ports:
+  for ( uint i = 0; i < N; i++ )
+  {
+    if ( !inport_enc[i].connected() )
+    {
+      log(Error)<<"ReadEncoders::Inputport not connected!"<<endlog();
+      return false;
+    }
+  }
+  if ( !outport.connected() ) {
+    log(Warning)<<"ReadEncoders::Outputport not connected!"<<endlog();
+  }
+  
+  sleep(1); // This is to ensure soem is already sending correct data. Might be much less than one second.
+  for ( uint i = 0; i < N; i++ )
+  {
+    // Initialising variables
+    ienc[i] = 0;
+    previous_enc_position[i] = 0.0; // obsolete
+    init_SI_value[i] = 0.0;
+    init_SI_value[i] = readEncoder(i);
+
+    // Setting first encoder value so the output always starts at zero
+  }
+  return true;
+}
+
+void ReadEncoders::updateHook()
+{
+    for ( uint i = 0; i < N; i++ )
+    {
+      SI_value[i] = readEncoder(i);
+    }
+  outport.write(SI_value);
+  counter++;
+}
+
+double ReadEncoders::readEncoder( int i )
+{
+  //log(Debug)<<"Reading encoder "<<i<<endlog();
+  EncoderMsg encdata;
+  inport_enc[i].read(encdata);
+  uint new_enc_position = encdata.value;
+  if( (previous_enc_position[i] - new_enc_position) > encoderbits/2)
+    ienc[i]++;
+  else if( (previous_enc_position[i] - new_enc_position) < (-1 * (double)encoderbits/2))
+    ienc[i]--;
+  previous_enc_position[i] = new_enc_position;
+  int enc_position = ienc[i] * encoderbits + new_enc_position;
+  double SI_value =  ((double)enc_position * enc2SI[i]) - init_SI_value[i];
+  return SI_value;
+}
+
+ORO_CREATE_COMPONENT(ReadEncoders)
---
+++ rtt_soem_interfacing/src/ReadEncoders.hpp
@@ -0,0 +1,65 @@
+#ifndef READENCODERS_HPP
+#define READENCODERS_HPP
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <ocl/Component.hpp>
+#include <soem_beckhoff_drivers/EncoderMsg.h>
+
+
+
+template <class T>
+inline string to_string (const T& t){
+  stringstream ss;
+  ss << t;
+  return ss.str();
+};
+
+
+#define maxN 10 //Maximum number of ports that can be created. Still a workaround.
+
+using namespace RTT;
+using namespace soem_beckhoff_drivers;
+
+namespace AMIGO // Just because it looks nice
+{
+  typedef vector<double> doubles;
+
+  class ReadEncoders
+  : public RTT::TaskContext
+    {
+    private:
+    // Number of encoders to read
+    uint N;
+
+    // Counter
+    uint counter;
+
+    // Declaring input- and output_ports
+    InputPort<EncoderMsg> inport_enc[maxN];
+    OutputPort<doubles> outport;
+
+    // Declaring message types
+    double previous_enc_position[maxN];
+    doubles init_SI_value;
+    int ienc[maxN];
+    doubles enc2SI;
+    uint encoderbits;
+    doubles SI_value;
+
+
+    public:
+
+    ReadEncoders(const string& name);
+    ~ReadEncoders();
+
+    bool configureHook();
+    bool startHook();
+    void updateHook();
+
+    private:
+
+    double readEncoder( int i );
+    };
+}
+#endif
