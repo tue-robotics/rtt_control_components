4839455c190551f4ff510b46040fd954c0a341f2
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2011-11-01 21:11:07 +0000

    Added ros interfacing
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/rtt_generic_components@5915 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ rtt_ROS_interfacing/src/BoolToROS.cpp
@@ -0,0 +1,60 @@
+/** BoolToROS.cpp
+ *
+ * @class BoolToROS
+ *
+ * \author Tim Clephas
+ * \date Istanbul, 2011
+ * \version 2.0
+ *
+ */
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <ocl/Component.hpp>
+
+#include "BoolToROS.hpp"
+
+using namespace RTT;
+using namespace ROS;
+
+BoolToROS::BoolToROS(const string& name) :
+	TaskContext(name, PreOperational)
+{
+	// Adding ports
+	Nbool = 1;
+	addProperty( "NumberOfBoolPorts", Nbool );
+}
+
+BoolToROS::~BoolToROS(){}
+
+bool BoolToROS::configureHook()
+{
+	for ( uint i = 0; i < Nbool; i++ )
+	{
+		string name_inport = "bool_in"+to_string(i+1);
+		string name_outport = "bool_out"+to_string(i+1);
+		log(Info)<<"Trying to create port "<<name_inport<<endlog();
+		addPort( name_inport, boolinports[i] );
+		addPort( name_outport, booloutports[i] );
+	}
+	return true;
+}
+
+bool BoolToROS::startHook()
+{
+	return true;
+}
+
+void BoolToROS::updateHook()
+{
+	for ( uint i = 0; i < Nbool; i++ )
+	{
+		bool value;
+		boolinports[i].read( value );
+		std_msgs::Bool msg;
+		msg.data = value;
+		booloutports[i].write( msg );
+	}
+}
+
+ORO_CREATE_COMPONENT(ROS::BoolToROS)
---
+++ rtt_ROS_interfacing/src/BoolToROS.hpp
@@ -0,0 +1,66 @@
+/** BoolToROS.hpp
+ *
+ * @class BoolToROS
+ *
+ * \author Tim Clephas
+ * \date Sept, 2011
+ * \version 1.0
+ *
+ */
+
+#ifndef BOOLTOROS_HPP
+#define BOOLTOROS_HPP
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <std_msgs/Bool.h>
+
+#define maxN 40 //Maximum  size. Still a workaround.
+
+template <class T>
+inline string to_string (const T& t){
+  stringstream ss;
+  ss << t;
+  return ss.str();
+};
+
+using namespace RTT;
+
+namespace ROS
+{
+  /**
+   * @brief
+   *
+   * @param No parameters
+   */
+
+  class BoolToROS
+  : public RTT::TaskContext
+    {
+    private:
+
+    typedef vector<double> doubles;
+    typedef vector<string> strings;
+
+    /* Declaring and output port*/
+    OutputPort<std_msgs::Bool> booloutports[maxN];
+
+    InputPort<bool> boolinports[maxN];
+
+
+    /* Declaring global variables */
+    uint Nbool; // Number of booleans to output
+
+
+    public:
+
+    BoolToROS(const string& name);
+    ~BoolToROS();
+
+    bool configureHook();
+    bool startHook();
+    void updateHook();
+
+    };
+}
+#endif
