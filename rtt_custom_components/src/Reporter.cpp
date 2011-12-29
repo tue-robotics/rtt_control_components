4c58ffdc38e040ebf94ce57bb66224d25b65f4fc
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2011-12-29 19:13:23 +0000

    Created reporter component
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/rtt_generic_components@6334 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ rtt_custom_components/src/Reporter.cpp
@@ -0,0 +1,99 @@
+/** Reporter.cpp
+*
+* @class Reporter
+*
+* \author Tim Clephas
+* \date December, 2011
+* \version 1.0
+*
+*/
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <ocl/Component.hpp>
+
+
+#include "Reporter.hpp"
+
+using namespace RTT;
+using namespace CUSTOM;
+
+Reporter::Reporter(const string& name) :
+	TaskContext(name, PreOperational),
+		N(2)
+{
+	addProperty( "number_of_inputs", N ).doc("An unsigned integer that specifies the number of input ports");;
+	addProperty( "ReportFile", filename ).doc("Name of the report file");;
+}
+
+Reporter::~Reporter(){}
+
+bool Reporter::configureHook()
+{
+	Logger::In in("Reporter::configureHook()");
+
+	addEventPort( "trigger", inports[0] );
+	for ( uint i = 1; i <= N; i++ )
+	{
+		string name_inport = "in"+to_string(i);
+		addPort( name_inport, inports[i] );
+	}
+
+	return true;
+}
+
+bool Reporter::startHook()
+{
+  Logger::In in("Reporter::startHook()");
+
+  // Check validity of Ports:
+  for (uint i = 0; i < N; i++) {
+	  if ( !inports[i].connected() ) {
+		  log(Error)<<"Input port "<< i <<" not connected!"<<endlog();
+		  // No connection was made, can't do my job !
+		  return false;
+	  }
+  }
+
+  if (N < 1 ) {
+    log(Error)<<"Reporter parameters not valid!"<<endlog();
+    return false;
+  }
+
+  // Create file
+  file.open (filename);
+
+  starttime = os::TimeService::Instance()->getTicks();
+  return true;
+}
+
+void Reporter::updateHook()
+{
+  Logger::In in("Reporter::updateHook()");
+
+  doubles output;
+  double vector_size = 0;
+
+  for ( uint i = 1; i <= N; i++ ) {
+	  doubles input;
+	  inports[i].read( input );
+	  vector_size = input.size();
+	  for ( uint j = 0; j < vector_size; j++ ) {
+		  output.push_back(input[j]);
+	  }
+  }
+
+
+  timestamp = os::TimeService::Instance()->secondsSince( starttime );
+  file << timestamp;
+  for ( uint i = 1; i <= output.size(); i++ ) {
+		file << " " << output[i];
+  }
+  file << "\n";
+}
+
+void Reporter::stopHook()
+{
+	file.close();
+}
+ORO_CREATE_COMPONENT(CUSTOM::Reporter)
