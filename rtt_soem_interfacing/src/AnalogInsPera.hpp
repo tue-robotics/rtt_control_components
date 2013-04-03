1968fb3f257d8cae8dfa6d448cf958e5e24e455f
Max Baeten <m.j.j.baeten@student.tue.nl>
2013-04-03 17:51:29 +0000

    pushed AnalogOutsPera and AnalogInsPera to trunk, not yet in CMakelists
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/rtt_generic_components@14236 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ rtt_soem_interfacing/src/AnalogInsPera.hpp
@@ -0,0 +1,54 @@
+#ifndef AnalogInsPera_HPP
+#define AnalogInsPera_HPP
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <soem_beckhoff_drivers/AnalogMsg.h>
+
+using namespace std;
+
+using namespace RTT;
+
+namespace SOEM
+{
+  typedef std::vector<double> doubles;
+
+  class AnalogInsPera
+  : public RTT::TaskContext
+    {
+	
+    private:
+
+    // declaring of ports
+    InputPort<soem_beckhoff_drivers::AnalogMsg> inport_for1;
+    InputPort<soem_beckhoff_drivers::AnalogMsg> inport_for2;
+    InputPort<soem_beckhoff_drivers::AnalogMsg> inport_for3;
+    InputPort<soem_beckhoff_drivers::AnalogMsg> inport_pos1;
+    InputPort<soem_beckhoff_drivers::AnalogMsg> inport_pos2;
+    InputPort<soem_beckhoff_drivers::AnalogMsg> inport_pos3;
+    OutputPort<doubles> outport_for;
+    OutputPort<doubles> outport_pos;
+
+    // declaring of msgs
+    soem_beckhoff_drivers::AnalogMsg amsgf1;
+    soem_beckhoff_drivers::AnalogMsg amsgf2;
+    soem_beckhoff_drivers::AnalogMsg amsgf3;
+    soem_beckhoff_drivers::AnalogMsg amsgp1;
+    soem_beckhoff_drivers::AnalogMsg amsgp2;
+    soem_beckhoff_drivers::AnalogMsg amsgp3;
+
+    // declaring of output doubles
+    doubles output_for;
+    doubles output_pos;
+	
+    public:
+
+    AnalogInsPera(const string& name);
+    ~AnalogInsPera();
+
+    bool configureHook();
+    bool startHook();
+    void updateHook();
+    };
+}
+#endif
