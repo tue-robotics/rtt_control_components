e92f3bb330cf9c0fff8688d507e715c47a8bb0b1
Max Baeten <m.j.j.baeten@student.tue.nl>
2013-06-18 08:45:24 +0000

    added switch component, commented out in the CMakelist since it has not yet been used or tested
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/rtt_generic_components@16773 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ rtt_routing_components/src/Switch.cpp
@@ -0,0 +1,93 @@
+/** Switch.hpp
+ *
+ * @class Switch
+ *
+ * \author Max Baeten
+ * \date June, 2013
+ * \version 1.0     Note that this component is not tested, and never used at this point 
+ *
+ */
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <rtt/Component.hpp>
+
+#include "Switch.hpp"
+
+using namespace std;
+using namespace RTT;
+using namespace SIGNALROUTING;
+
+Switch::Switch(const string& name) :
+    TaskContext(name, PreOperational),
+    N(1)
+{
+  addProperty( "size_of_input", N ).doc("An unsigned integer that specifies the size of the doubles that are sent over the three ports. (same for all )");;
+}
+
+Switch::~Switch(){}
+
+bool Switch::configureHook()
+{
+    log(Warning)<<"Switch: Start of configureHook()"<<endlog();
+	addEventPort( "in_def", inport_default );
+	addEventPort( "in_swi", inport_switched );
+	addPort( "switchParam", inport_switchParam );
+	addPort( "out", outport );
+	
+    switch_param = false;
+    input_default.assign(N,0.0);
+    input_switched.assign(N,0.0);
+    output.assign(N,0.0);
+    log(Warning)<<"Switch: End of configureHook()"<<endlog();
+	return true;
+}
+
+bool Switch::startHook()
+{
+  log(Warning)<<"Switch: Start of startHook()"<<endlog();
+
+  if ( !inport_default.connected() ) {
+    log(Error)<<"Switch: inport_default not connected!"<<endlog();
+    return false;
+  }
+  
+  if ( !inport_switched.connected() ) {
+    log(Error)<<"Switch: inport_switched not connected!"<<endlog();
+    return false;
+  }
+  
+  if ( !outport.connected() ) {
+    log(Error)<<"Switch: Outputport not connected!"<<endlog();
+    return false;
+  }
+
+  log(Warning)<<"Switch: End of startHook()"<<endlog();
+  return true;
+}
+
+void Switch::updateHook()
+{
+  inport_default.read(input_default);
+  inport_switched.read(input_switched);
+	
+  if ( NewData == inport_switchParam.read(switch_param))
+  {
+	  log(Warning)<< "Switch: New SwitchingParam received" << endlog(); // TO DO set to info
+  }
+  
+  if (switch_param == false )
+  {
+	  output = input_default;
+  }
+	
+  if (switch_param == true )
+  {
+	  output = input_switched;
+  }
+
+
+  outport.write( output );
+}
+
+ORO_CREATE_COMPONENT(SIGNALROUTING::Switch)
---
+++ rtt_routing_components/src/Switch.hpp
@@ -0,0 +1,68 @@
+/** Switch.hpp
+ *
+ * @class Switch
+ *
+ * \author Max Baeten
+ * \date June, 2013
+ * \version 1.0     Note that this component is not tested, and never used at this point 
+ *
+ */
+
+#ifndef Switch_HPP
+#define Switch_HPP
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#define maxN 10 //Maximum number of ports that can be created. Still a workaround.
+
+using namespace std;
+using namespace RTT;
+namespace SIGNALROUTING
+{
+  // Define a new type for easy coding:
+  typedef vector<double> doubles;
+  
+    /**
+   * @brief A Component that Switches the input signals, i.e. 
+   *        switches two input signals to one port. Usefull for
+   * 		switching of Homing component and ReadReference component
+   * 
+   * 		This component switches from port in_default
+   * 		to port in_switched, 
+   *		when switch_param becomes true
+   * 
+   */
+
+  class Switch
+  : public RTT::TaskContext
+    {
+    private:
+
+		// Declaring and output ports
+        InputPort<bool> inport_switchParam; //to Do make service call not port
+		InputPort<doubles> inport_default;
+		InputPort<doubles> inport_switched;
+		OutputPort<doubles> outport;
+		
+		bool switch_param;
+        uint N;
+		doubles input_default;
+        doubles input_switched;
+		doubles output;
+		
+    public:
+
+		Switch(const string& name);
+		~Switch();
+
+		bool configureHook();
+		bool startHook();
+		void updateHook();
+
+    };
+}
+#endif
