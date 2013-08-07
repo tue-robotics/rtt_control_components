90b86d3fd4efb9825b2e65e678a411ffc0fa5034
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2013-08-07 15:04:39 +0000

    Added admittance controllers
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/rtt_generic_components@17905 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ rtt_filter_components/src/AdmittanceControllers.cpp
@@ -0,0 +1,155 @@
+/** AdmittanceControllers.cpp
+*
+* @class AdmittanceControllers
+*
+* \author Janno Lunenburg
+* \date August, 2013
+* \version 1.0
+*
+*/
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <rtt/Component.hpp>
+
+#include "AdmittanceControllers.hpp"
+
+#define PI 3.14159265358979
+#define eps 1e-16
+
+using namespace std;
+using namespace RTT;
+using namespace FILTERS;
+
+AdmittanceControllers::AdmittanceControllers(const string& name) :
+    TaskContext(name, PreOperational),
+    vector_size(0), Ts(0.0)
+{
+
+    addProperty( "vector_size", vector_size );
+    addProperty( "sampling_time", Ts );
+    addProperty( "masses", M );
+    addProperty( "damping_coefficients", D );
+    addProperty( "lower_joint_limits", lower_bound );
+    addProperty( "upper_joint_limits", upper_bound );
+
+    // Adding ports
+    addEventPort( "measured_position_in", position_inport );
+    addPort( "force_in", force_inport );
+    addPort( "out", outport );
+}
+
+AdmittanceControllers::~AdmittanceControllers()
+{
+    for (unsigned int i = 0; i < vector_size; i++)
+    {
+        delete filters[i];
+    }
+}
+
+bool AdmittanceControllers::configureHook()
+{
+    // Compute fp and dp for first order lowpass filter
+    // and Construct filters
+    fp.resize(vector_size);
+    k.resize(vector_size);
+    filters.resize(vector_size);
+    for (unsigned int i = 0; i < vector_size; i++)
+    {
+        k[i] = 1/D[i];
+        double op = D[i]/M[i]; //Pole frequency in rad/s
+        fp[i] = op/(2*PI);
+        filters[i] = new DFILTERS::DFirstOrderLowpass(fp[i], Ts);
+    }
+
+    force_input.assign(vector_size, 0.0);
+    position_input.assign(vector_size, 0.0);
+    position_output.assign(vector_size, 0.0);
+
+    return true;
+}
+
+bool AdmittanceControllers::startHook()
+{
+    // Check validity of Ports:
+    if ( !position_inport.connected() ) {
+        log(Error)<<"AdmittanceControllers::position_inputport not connected!"<<endlog();
+        // No connection was made, can't do my job !
+        return false;
+    }
+
+    if ( !force_inport.connected() ) {
+        log(Error)<<"AdmittanceControllers::force_inputport not connected!"<<endlog();
+        // No connection was made, can't do my job !
+        return false;
+    }
+
+    if ( !outport.connected() ) {
+        log(Warning)<<"AdmittanceControllers::Outputport not connected!"<<endlog();
+    }
+
+    if (vector_size < 1 || Ts <= 0.0) {
+        log(Error)<<"AdmittanceControllers parameters not valid!"<<endlog();
+        return false;
+    }
+
+    for (uint i = 0; i < vector_size; i++) {
+        if (fp[i] <= 0.0 || k[i] <= 0.0){
+            log(Error)<<"AdmittanceControllers parameters not valid!"<<endlog();
+            return false;
+        }
+    }
+
+    // Read ports to initialize
+    force_inport.read(force_input);
+    position_inport.read(position_input);
+    return true;
+}
+
+void AdmittanceControllers::updateHook()
+{
+    // Read inports
+    position_inport.read(position_input);
+    if (force_inport.read(force_input) == NewData)
+    {
+        timestamp_last_force_input = os::TimeService::Instance()->getNSecs()*1e-9;
+    }
+
+    // Safety check
+    long double current_timestamp = os::TimeService::Instance()->getNSecs()*1e-9;
+    double duration = (double)(current_timestamp - timestamp_last_force_input);
+    bool force_input_safe = true;
+    for (unsigned int i = 0; i < vector_size; i++)
+    {
+        // If: ( do_it_only_once && force is unequal to zero && time since last update exceeds threshold )
+        if ( force_input_safe && fabs(force_input[i]) > eps && duration > 0.11)
+        {
+            force_input_safe = false;
+            log(Warning) << "No new force input received for " << duration << "seconds, setting force to zero" << endlog();
+            force_input.assign(vector_size, 0.0);
+        }
+    }
+
+    // Update stuff
+    for (unsigned int i = 0; i < vector_size; i++)
+    {
+        // ToDo: is it more efficient to do this (less readable) in less lines?
+
+        // Update
+        filters[i]->update(force_input[i]);
+        double filter_out = filters[i]->getOutput();
+        double v_desired = k[i] * filter_out;
+
+        // Integrate
+        position_output[i] = position_input[i] + v_desired * Ts;
+
+        // Saturate
+        position_output[i] = max(position_output[i], lower_bound[i]);
+        position_output[i] = min(position_output[i], upper_bound[i]);
+    }
+
+    // Write results
+    outport.write(position_output);
+}
+
+ORO_CREATE_COMPONENT(FILTERS::AdmittanceControllers)
---
+++ rtt_filter_components/src/AdmittanceControllers.hpp
@@ -0,0 +1,99 @@
+/** AdmittanceControllers.hpp
+ *
+ * @class AdmittanceControllers
+ *
+ * \author Janno Lunenburg
+ * \date August, 2013
+ * \version 1.0
+ *
+ */
+
+#ifndef ADMITTANCECONTROLLERS_HPP
+#define ADMITTANCECONTROLLERS_HPP
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <vector>
+
+#include <scl/filters/DFirstOrderLowpass.hpp>
+
+using namespace std;
+using namespace RTT;
+
+namespace FILTERS
+{
+  
+  // Define a new type for easy coding:
+  typedef vector<double> doubles;
+	
+  /**
+   * @brief A Component that acts as an admittance controller.
+   *
+   * Based on a virtual masses M and damper constants D, it computes a desired velocity v_r
+   * for the input force F
+   * using the model M (dv_r/dt) + D v_r = F.
+   * This is equal to a first order lowpass filter with gain = 1/D and pole at D/M
+   *
+   * Finally, it computes the new position reference using x_r = x + v_r *dt
+   *
+   * @param * M virtual mass or inertia
+   *        * D virtual damping coefficient
+   */
+   
+  class AdmittanceControllers
+  : public RTT::TaskContext
+    {
+    private:
+
+		/* Declaring input and output ports*/
+        InputPort<doubles> position_inport;
+        InputPort<doubles> force_inport;
+		OutputPort<doubles> outport;
+
+		/* Declaring global variables */
+		// Variables for history storage
+        doubles force_input;
+        doubles position_input;
+        doubles position_output;
+        long double timestamp_last_force_input;
+
+        //doubles previous_output;
+        //doubles previous_input;
+        //doubles second_previous_output;
+        //doubles second_previous_input;
+
+		// Numerator and denominator of the filter
+        //doubles a0;
+        //doubles a1;
+        //doubles a2;
+        //doubles b0;
+        //doubles b1;
+        //doubles b2;
+		
+		/* Declaring variables set by properties */
+		// Filter parameters
+        doubles M;
+        doubles D;
+        doubles fp;
+        doubles k;
+        uint vector_size;
+        double Ts;
+
+        // Joint limits
+        doubles lower_bound;
+        doubles upper_bound;
+
+        vector<DFILTERS::DFirstOrderLowpass*> filters;
+		
+    public:
+
+        AdmittanceControllers(const string& name);
+        ~AdmittanceControllers();
+
+		bool configureHook();
+		bool startHook();
+		void updateHook();
+
+    };
+}
+#endif
