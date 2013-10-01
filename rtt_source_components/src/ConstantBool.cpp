e86d6b366d2d94e9c085c413942619b9acf2eb92
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2013-10-01 13:05:01 +0000

    Added ConstantBool component
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/rtt_generic_components@18757 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ rtt_source_components/src/ConstantBool.cpp
@@ -0,0 +1,63 @@
+/** ConstantBool.cpp
+*
+* @class ConstantBool
+*
+* \author Janno Lunenburg
+* \date September, 2013
+* \version 1.0
+*
+*/
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <rtt/Component.hpp>
+
+#include "ConstantBool.hpp"
+
+using namespace std;
+using namespace RTT;
+using namespace SOURCES;
+
+ConstantBool::ConstantBool(const string& name) : 
+	TaskContext(name, PreOperational)
+{
+  addProperty( "value", value ).doc("A bool, that determines  whether the output is true or false");
+}
+
+ConstantBool::~ConstantBool(){}
+
+bool ConstantBool::configureHook()
+{
+  Logger::In in("ConstantSignal::configureHook()");
+  
+  // Adding ports
+  addPort( "out", outport ).doc("Bool");
+
+  Ts = getPeriod();
+  
+  return true;
+}
+
+bool ConstantBool::startHook()
+{
+  Logger::In in("ConstantSignal::startHook()");
+  
+  if ( !outport.connected() ) {
+    log(Warning)<<"Output port not connected!"<<endlog();
+  }
+
+  if (Ts <= 0.0) {
+    log(Error)<<"Period of the component not valid!"<<endlog();
+    return false;
+  }
+
+  return true;
+}
+
+void ConstantBool::updateHook()
+{
+  // Write the outputs
+  outport.write( value );
+}
+
+ORO_CREATE_COMPONENT(SOURCES::ConstantBool)
---
+++ rtt_source_components/src/ConstantBool.hpp
@@ -0,0 +1,56 @@
+/** ConstantBool.hpp
+ *
+ * @class ConstantBool
+ *
+ * \author Janno Lunenburg
+ * \date September, 2013
+ * \version 1.0
+ *
+ */
+
+#ifndef CONSTANTBOOL_HPP
+#define CONSTANTBOOL_HPP
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+
+using namespace std;
+using namespace RTT;
+
+namespace SOURCES
+{
+  
+  /**
+   * @brief A Component that generates constant boolean signal
+   *
+   * The component has no input ports and N output ports with vector of
+   * doubles
+   *
+   * @param * value [-] - constant value of the generated signal: true or false
+   */
+   
+  class ConstantBool
+  : public RTT::TaskContext
+    {
+    private:
+
+		/* Declaring and output port*/
+		OutputPort<bool> outport;
+
+		double Ts;
+
+		/* Declaring variables set by properties */
+		bool value;
+		
+    public:
+
+		ConstantBool(const string& name);
+		~ConstantBool();
+
+		bool configureHook();
+		bool startHook();
+		void updateHook();
+
+    };
+}
+#endif
