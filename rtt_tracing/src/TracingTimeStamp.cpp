d648485713b16bc2dfb996e2f6ed89b0777fef9f
Ton Peters <peterstmc@gmail.com>
2014-05-13 13:30:13 +0000

    tracing componenet TracingTimeStamp working correctly
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/rtt_generic_components@24242 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- rtt_tracing/src/TracingTimeStamp.cpp
+++ rtt_tracing/src/TracingTimeStamp.cpp
@@ -1,6 +1,6 @@
-/** TracingOneSignal.cpp
+/** TracingTimeStamp.cpp
  *
- * @class Tracing
+ * @class TracingTimeStamp
  *
  * \author Tim Clephas
  * \date Summer, 2012
@@ -14,13 +14,13 @@
 #include <fstream>
 
 
-#include "TracingOneSignal.hpp"
+#include "TracingTimeStamp.hpp"
 
 using namespace std;
 using namespace RTT;
 using namespace Signal;
 
-TracingOneSignal::TracingOneSignal(const string& name) :
+TracingTimeStamp::TracingTimeStamp(const string& name) :
 											TaskContext(name, PreOperational),
 											filename("data.txt"),
 											buffersize(16384),
@@ -32,30 +32,25 @@ TracingOneSignal::TracingOneSignal(const string& name) :
 	addProperty( "Ts", Ts ).doc("Sample time of orocos, used for time vector");
 }
 
-TracingOneSignal::~TracingOneSignal(){}
+TracingTimeStamp::~TracingTimeStamp(){}
 
-bool TracingOneSignal::configureHook()
+bool TracingTimeStamp::configureHook()
 {
 	columns = 0;
 
 	// Create ports based on the number of vector sizes given
-    Nports = vectorsizes_prop.size()+1;
+    Nports = vectorsizes_prop.size();
 	vectorsizes.resize(Nports);
 	counters.resize(Nports);
 
-    // Creating time slot
-    vectorsizes[0] = 1.0;
-    counters[0] = 0;
-    columns = 1;
-
 
 	// Creating ports
-    for ( uint i = 1; i < Nports; i++ )
+    for ( uint i = 0; i < Nports; i++ )
 	{
-		vectorsizes[i] = vectorsizes_prop[i-1]; // Hack
+		vectorsizes[i] = vectorsizes_prop[i]+1; // Hack
 		counters[i] = 0;
-        string name_inport = "in"+to_string(i);
-		addEventPort( name_inport, inports[i-1]);
+        string name_inport = "in"+to_string(i+1);
+		addEventPort( name_inport, inports[i]);
 		columns += vectorsizes[i];
 		cout << "Column size: ";
 		cout << columns;
@@ -74,51 +69,51 @@ bool TracingOneSignal::configureHook()
 	return true;
 }
 
-bool TracingOneSignal::startHook()
+bool TracingTimeStamp::startHook()
 {
 	/*if ( !inports[0].connected() ) {
 		log(Error)<<"Input port not connected!"<<endlog();
 		return false;
 	}*/
-	current_ticks = 0;
-    previous_ticks = RTT::os::TimeService::Instance()->getTicks();
+    start_ticks = RTT::os::TimeService::Instance()->getTicks();
 	return true;
 }
 
-void TracingOneSignal::updateHook()
+void TracingTimeStamp::updateHook()
 {
 	// First updatehook is useless
 	if(counter == -1){counter = 0;return;}
 	// TODO: if ( !this->isRunning() ) return; (Check if this also works, counter can become a uint
 
 	uint startcolumn = 0;
-    for ( uint i = 1; i < Nports; i++ )
+    for ( uint i = 0; i < Nports; i++ )
 	{
-        doubles input(vectorsizes[i],-2.0);
+		// get the time
+		current_seconds = RTT::os::TimeService::Instance()->secondsSince(start_ticks);
         
-        // Save time stamp
-		if ( startcolumn == 0 )
+        doubles input(vectorsizes[i]-1,-2.0);
+        
+		if ( inports[i].read( input ) == NewData && counters[i] <= counter )
 		{
-			buffers[counters[0]][startcolumn] = RTT::os::TimeService::Instance()->secondsSince(previous_ticks);
-			startcolumn += vectorsizes[0];
-			counters[0]++;
-		}
-
-		if ( inports[i-1].read( input ) == NewData && counters[i] <= counter )
-		{
-            
-
+					
+			// place time stamp
+			buffers[counters[i]][startcolumn] = current_seconds;
+			startcolumn++;
+			
 			uint inputiterator = 0;
 			// Fill it with data
-			for (uint column =  startcolumn; column < startcolumn + vectorsizes[i]; ++column)
+			for (uint column =  startcolumn; column < startcolumn + vectorsizes[i]-1; ++column)
 			{
 				buffers[counters[i]][column] = input[inputiterator];
 				inputiterator++;
 			}
-
+			counters[i]++;
+			startcolumn += vectorsizes[i]-1;
+		} 
+		else
+		{
+			startcolumn += vectorsizes[i];
 		}
-        counters[i]++;
-		startcolumn += vectorsizes[i];
 	}
 
 	counter = *max_element(counters.begin(),counters.end());
@@ -130,19 +125,19 @@ void TracingOneSignal::updateHook()
 	}
 }
 
-void TracingOneSignal::stopHook()
+void TracingTimeStamp::stopHook()
 {
 	FILE * pFile;
 	pFile = fopen (filename.c_str(),"w");
 
 
-    fprintf(pFile, "SecondsPast\t");
-	for ( uint i = 1; i < vectorsizes.size(); i++ )
+    fprintf(pFile, "Sample_i\t");
+	for ( uint i = 0; i < vectorsizes.size(); i++ )
 	{
-		string portname = inports[i-1].getName();
+		string portname = inports[i].getName();
 		for ( int j = 0; j < vectorsizes[i]; j++ )
 		{
-            fprintf(pFile, "%s[%d]\t", portname.c_str(), j );
+            fprintf(pFile, "%s_%d\t", portname.c_str(), j );
 			//char columnheader = "["+to_string(j)+"]\t";
 			//portname.c_str()+
 			//fprintf(pFile, "%s\t", columnheader);
@@ -150,19 +145,20 @@ void TracingOneSignal::stopHook()
 		}
 	}
 	fprintf(pFile, "\n");
-	//
-
+	
+	
 	uint line = 0;
 	std::vector<doubles>::iterator vit;
 	doubles::iterator dit;
 	for(vit = buffers.begin(); vit != buffers.end(); ++vit)
 	{
-        //double timevalue = line*Ts;
-        //fprintf(pFile, "%12.6e\t", timevalue);
+		double timevalue = line;
+        fprintf(pFile, "%12.6e\t", timevalue);
 		for(dit = (*vit).begin(); dit != (*vit).end(); ++dit)
 		{
             fprintf(pFile, "%12.6e\t", *dit);
 			//fprintf(pFile, "% f\t", *dit);
+			
 		}
 		fprintf(pFile, "\n");
 		line++;
@@ -175,4 +171,4 @@ void TracingOneSignal::stopHook()
 
 }
 
-ORO_CREATE_COMPONENT(Signal::TracingOneSignal)
+ORO_CREATE_COMPONENT(Signal::TracingTimeStamp)
---
+++ rtt_tracing/src/TracingTimeStamp.hpp
@@ -0,0 +1,89 @@
+/** TracingTimeStamp.hpp
+ *
+ * @class TracingTimeStamp
+ *
+ * \author Tim Clephas
+ * \date Summer, 2012
+ * \version 1.0
+ *
+ */
+
+#ifndef TracingTimeStamp_HPP
+#define TracingTimeStamp_HPP
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include "boost/multi_array.hpp"
+#include <cassert>
+
+#include <rtt/os/Timer.hpp>
+
+#define maxN 10 //Maximum number of ports that can be created. Still a workaround.
+
+
+using namespace std;
+using namespace RTT;
+
+namespace Signal
+{
+  // Define a new type for easy coding:
+  typedef vector<double> doubles;
+  typedef vector<uint> uints;
+
+  template <class T>
+  inline string to_string (const T& t){
+    stringstream ss;
+    ss << t;
+    return ss.str();
+  };
+  
+  /**
+   * @brief A Component that calculates Tracingolute value of the 
+   *        input signals
+   *
+   * The component has input port as stated in property number_of_inputs,
+   * that should receive vector of doubles.
+   * Input ports are eventports which will trigger the component.
+   * 
+   * @param * N - number of inputs
+   *        * vector_size - size of input vectors
+   */
+   
+  class TracingTimeStamp
+  : public RTT::TaskContext
+    {
+    private:
+
+		/* Declaring and output port*/
+		InputPort<doubles> inports[maxN];
+
+		string filename;
+		doubles vectorsizes_prop;
+		vector<int> vectorsizes;
+		uint buffersize;
+		double Ts;
+
+		doubles buffer;
+		vector<doubles> buffers;
+		uints counters;
+		int counter;
+		uint columns;
+		uint rows;
+		uint Nports;
+
+        RTT::os::TimeService::ticks start_ticks;
+        RTT::os::TimeService::Seconds current_seconds;
+	
+    public:
+
+        TracingTimeStamp(const string& name);
+        ~TracingTimeStamp();
+
+		bool configureHook();
+		bool startHook();
+		void updateHook();
+		void stopHook();
+
+    };
+}
+#endif
