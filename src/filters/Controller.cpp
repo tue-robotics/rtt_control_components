470cda9871e292c8de0839db943ebfad88fdedf1
Max Baeten <m.j.j.baeten@student.tue.nl>
2014-12-21 13:25:45 +0100

    clean up of controller and added multiple inports and addition of ffw's

--- src/filters/Controller.cpp
+++ src/filters/Controller.cpp
@@ -25,11 +25,14 @@ Controller::Controller(const string& name) :
 {
     // Properties
     addProperty("vector_size",                  vector_size)            .doc("Number of controllers");
-    addProperty("gains",                        gains)                  .doc("Gains");
+    addProperty("number_of_ffwports",           N_ffwinports)     .doc("The amount of feed forward ports");
     addProperty("controllers",                  controllers)            .doc("List of used controllers");
     addProperty("sampling_time",                Ts)                     .doc("Sampling time");
-    
-    // Properties Controllers (fill in only for the used filters)
+    addProperty("number_of_inports",            N_refinports)              .doc("Number of inports");
+    addProperty("inport_sizes",                 inport_sizes)           .doc("Sizes of inports");
+
+    // Controller Properties (You don't have to fill in the unused filters)
+    addProperty("gains",                        gains)                  .doc("Gains");
     addProperty("zero_freq_WeakIntegrator",     fz_WeakIntegrator)      .doc("zero frequency of the weak integrator");
     addProperty("zero_freq_LeadLag",            fz_LeadLag)             .doc("zero frequency of the lead lag filter");
     addProperty("pole_freq_LeadLag",            fp_LeadLag)             .doc("pole frequency of the lead lag filter");
@@ -41,7 +44,6 @@ Controller::Controller(const string& name) :
     addProperty("pole_damp_LowPass",            dp_LowPass)             .doc("pole damping of the low pass filter");
 
     // Adding ports
-    addPort( "ref_in",                          references_inport)      .doc("Control Reference port");
     addEventPort( "pos_in",                     positions_inport)       .doc("Position Port");
     addPort( "enable",                     		enable_inport)  		.doc("Receives enable boolean from safety component, controller sends out zeros if enable = false");
     addPort( "out",                             controleffort_outport)  .doc("Control output port");
@@ -73,6 +75,14 @@ Controller::~Controller()
 
 bool Controller::configureHook()
 {
+    // Add Inports and FFW ports
+    for ( uint j = 0; j < N_refinports; j++ ) {
+        addPort( ("ref_in"+to_string(j+1)),     references_inport[j] )      .doc("Control Reference port");
+    }
+    for (uint j = 0; j < N_ffwinports; j++) {
+        addPort( ("ffw_in" + to_string(j)),     ffw_inport[j])              .doc("One of the FFW inports");
+    }
+
     // Determine which controllers are demanded and declare their properties
     WeakIntegrator = false;
     LeadLag = false;
@@ -89,7 +99,8 @@ bool Controller::configureHook()
         } else if (controllers[i] == "LowPass") {
             LowPass = true;
         } else {
-            log(Error)<<"Controller: Controller number:" << i << " , " << controllers[i] << " is not supported! Choose from: [WeakIntegrator, LeadLag, Notch, LowPass]!"<<endlog();
+            log(Error)<<"Controller: Could not configure component: Controller number:" << i << " , " << controllers[i] << " is not supported! Choose from: [WeakIntegrator, LeadLag, Notch, LowPass]!"<<endlog();
+            return false;
         }
     }
 
@@ -125,53 +136,53 @@ bool Controller::configureHook()
 
     // Check input data sizes
     if ( gains.size() != vector_size ) {
-        log(Error)<<"Controller:: Wrong size of one of the gains!"<<endlog();
+        log(Error)<<"Controller: Could not configure component: Wrong size of one of the gains!"<<endlog();
         return false;
     }
     if ( WeakIntegrator && (fz_WeakIntegrator.size() != vector_size )) {
-        log(Error)<<"Controller:: Wrong size of one of the weak integrator!"<<endlog();
+        log(Error)<<"Controller: Could not configure component: Wrong size of one of the weak integrator!"<<endlog();
         return false;
     }
     if ( LeadLag && (fz_LeadLag.size() != vector_size || fp_LeadLag.size() != vector_size )) {
-        log(Error)<<"Controller:: Wrong size of one of the lead lag filter!"<<endlog();
+        log(Error)<<"Controller: Could not configure component: Wrong size of one of the lead lag filter!"<<endlog();
         return false;
     }
     if ( Notch && (fz_Notch.size() != vector_size || dz_Notch.size() != vector_size || fp_Notch.size() != vector_size || dp_Notch.size() != vector_size )) {
-        log(Error)<<"Controller:: Wrong size of one of the notch filter!"<<endlog();
+        log(Error)<<"Controller: Could not configure component: Wrong size of one of the notch filter!"<<endlog();
         return false;
     }
     if ( LowPass && (fp_LowPass.size() != vector_size || dp_LowPass.size() != vector_size )) {
-        log(Error)<<"Controller:: Wrong size of one of the low pass!"<<endlog();
+        log(Error)<<"Controller: Could not configure component: Wrong size of one of the low pass!"<<endlog();
         return false;
     }
 
     // Check input data signs
     if (vector_size < 1 || Ts <= 0.0) {
-        log(Error)<<"Controller:: vector_size or Ts parameter is invalid!"<<endlog();
+        log(Error)<<"Controller: Could not configure component: vector_size or Ts parameter is invalid!"<<endlog();
         return false;
     }
     for (uint i = 0; i < vector_size; i++) {
         if (WeakIntegrator) {
             if ( fz_WeakIntegrator[i] < 0.0) {
-                log(Error)<<"Controller:: Wrong sign of one of the weak integrator parameters!"<<endlog();
+                log(Error)<<"Controller: Could not configure component: Wrong sign of one of the weak integrator parameters!"<<endlog();
                 return false;
             }
         }
         if (LeadLag) {
             if ( fz_LeadLag[i] < 0.0 || fp_LeadLag[i] < 0.0 ) {
-                log(Error)<<"Controller:: Wrong sign of one of the leadlag parameters!"<<endlog();
+                log(Error)<<"Controller: Could not configure component: Wrong sign of one of the leadlag parameters!"<<endlog();
                 return false;
             }
         }
         if (Notch) {
             if ( fz_Notch[i] < 0.0 || dz_Notch[i] < 0.0 || fp_Notch[i] < 0.0 || dp_Notch[i] < 0.0) {
-                log(Error)<<"Controller:: Wrong sign of one of the notch parameters!"<<endlog();
+                log(Error)<<"Controller: Could not configure component: Wrong sign of one of the notch parameters!"<<endlog();
                 return false;
             }
         }
         if (LowPass) {
             if ( fp_LowPass[i] < 0.0 || dp_LowPass[i] < 0.0 ) {
-                log(Error)<<"Controller:: Wrong sign of one of the low pass parameters!"<<endlog();
+                log(Error)<<"Controller: Could not configure component: Wrong sign of one of the low pass parameters!"<<endlog();
                 return false;
             }
         }
@@ -183,17 +194,29 @@ bool Controller::configureHook()
 bool Controller::startHook()
 {
     // Check validity of Ports:
-    if ( !references_inport.connected() || !positions_inport.connected() ) {
-        log(Error)<<"Controller: One of the inports is not connected!"<<endlog();
+    if ( !positions_inport.connected() ) {
+        log(Error)<<"Controller: Could not start component: pos_in is not connected!"<<endlog();
         return false;
     }
-
+    
     if ( !controleffort_outport.connected() ) {
-        log(Error)<<"Controller: Outputport not connected!"<<endlog();
+        log(Error)<<"Controller: Could not start component:  Outputport not connected!"<<endlog();
         return false;
+    }    
+    for (uint j = 0; j < N_ffwinports; j++) {
+        if ( !ffw_inport[j].connected() ) {
+            log(Error)<<"Controller: Could not start component: ffw_in" << j + 1 << " not connected!"<<endlog();
+			return false;
+		}
+	}
+    for (uint j = 0; j < N_refinports; j++) {
+        if ( !references_inport[j].connected() ) {
+            log(Error)<<"Controller: Could not start component: ref_in" << j + 1 << " not connected!"<<endlog();
+            return false;
+        }
     }
 
-    // initialize variables
+    // Initialize
     safe = false;
 
     return true;
@@ -201,19 +224,29 @@ bool Controller::startHook()
 
 void Controller::updateHook()
 {
-    doubles references(vector_size,0.0);
+    // Read positions
     doubles positions(vector_size,0.0);
-    doubles jointErrors(vector_size,0.0);
-    doubles output_Gains(vector_size,0.0);
-    doubles output_WeakIntegrator(vector_size,0.0);
-    doubles output_LeadLag(vector_size,0.0);
-    doubles output_Notch(vector_size,0.0);
-    doubles output(vector_size,0.0);
-
-    // Read the input ports
-    references_inport.read(references);
     positions_inport.read(positions);
 
+    // Read all reference ports
+    doubles references(vector_size,0.0);
+    uint k = 0;
+    for ( uint j = 0; j < N_refinports; j++ ) {
+        doubles ref_in(inport_sizes[j],0.0);
+        references_inport[j].read(ref_in);
+        for ( uint i = 0; i < inport_sizes[j]; i++ ) {
+            references[k+i] = ref_in[i];
+        }
+    }
+
+    // Read (possible) ffw ports
+    vector<doubles> ffw_input;
+    for (uint j = 0; j < N_ffwinports; j++) {
+        ffw_input[j].assign(vector_size,0.0);
+        ffw_inport[j].read(ffw_input[j]);
+    }
+
+    // Read safety boolean
     enable_inport.read(safe);
 
     if (!safe) {
@@ -225,57 +258,55 @@ void Controller::updateHook()
         return ;
 
     } else {
-        // Compute joint errors
+
+        doubles jointErrors(vector_size,0.0);
+        doubles output_Gains(vector_size,0.0);
+        doubles output_WeakIntegrator(vector_size,0.0);
+        doubles output_LeadLag(vector_size,0.0);
+        doubles output_Notch(vector_size,0.0);
+        doubles output(vector_size,0.0);
+
         for (uint i = 0; i < vector_size; i++) {
+			// Compute joint errors and Apply Gain
             jointErrors[i] = references[i]-positions[i];
-        }
-        // Apply Gain
-        for (uint i = 0; i < vector_size; i++) {
             output_Gains[i] = jointErrors[i]*gains[i];
-        }
-        // Apply Weak Integrator
-        if (WeakIntegrator) {
-            for (uint i = 0; i < vector_size; i++) {
-                filters_WeakIntegrator[i]->update( output_Gains[i] );
-                output_WeakIntegrator[i] = filters_WeakIntegrator[i]->getOutput();
-            }
-        } else {
-            for (uint i = 0; i < vector_size; i++) {
-                output_WeakIntegrator[i] = output_Gains[i];
-            }
-        }
-        // Apply Lead Lag
-        if (LeadLag) {
-            for (uint i = 0; i < vector_size; i++) {
-                filters_LeadLag[i]->update( output_WeakIntegrator[i] );
-                output_LeadLag[i] = filters_LeadLag[i]->getOutput();
-            }
-        } else {
-            for (uint i = 0; i < vector_size; i++) {
-                output_LeadLag[i] = output_WeakIntegrator[i];
-            }
-        }
-        // Apply Notch
-        if (Notch) {
-            for (uint i = 0; i < vector_size; i++) {
-                filters_Notch[i]->update( output_LeadLag[i] );
-                output_Notch[i] = filters_Notch[i]->getOutput();
-            }
-        } else {
-            for (uint i = 0; i < vector_size; i++) {
-                output_Notch[i] = output_LeadLag[i];
-            }
-        }
-        // Apply Low Pass
-        if (LowPass) {
-            for (uint i = 0; i < vector_size; i++) {
-                filters_LowPass[i]->update( output_Notch[i] );
-                output[i] = filters_LowPass[i]->getOutput();
-            }
-        } else {
-            for (uint i = 0; i < vector_size; i++) {
-                output[i] = output_Notch[i];
-            }
+        
+			// Apply Weak Integrator
+			if (WeakIntegrator) {
+				filters_WeakIntegrator[i]->update( output_Gains[i] );
+				output_WeakIntegrator[i] = filters_WeakIntegrator[i]->getOutput();
+			} else {
+				output_WeakIntegrator[i] = output_Gains[i];
+			}
+			
+			// Apply Lead Lag
+			if (LeadLag) {
+				filters_LeadLag[i]->update( output_WeakIntegrator[i] );
+				output_LeadLag[i] = filters_LeadLag[i]->getOutput();
+			} else {
+				output_LeadLag[i] = output_WeakIntegrator[i];
+			}
+			
+			// Apply Notch
+			if (Notch) {
+				filters_Notch[i]->update( output_LeadLag[i] );
+				output_Notch[i] = filters_Notch[i]->getOutput();
+			} else {
+				output_Notch[i] = output_LeadLag[i];
+			}
+			
+			// Apply Low Pass
+			if (LowPass) {
+				filters_LowPass[i]->update( output_Notch[i] );
+				output[i] = filters_LowPass[i]->getOutput();
+			} else {
+				output[i] = output_Notch[i];
+			}
+
+			// Add possible feed forward inputs to the controller 
+            for (uint j = 0; j < N_ffwinports; j++) {
+                output[i] += ffw_input[j][i];
+			}
         }
 
         // Write the outputs
