3a3d02e879acaca69c1541a7052088b3801912af
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2015-03-25 01:55:31 +0100

    Test without bodypart namespacing

--- test/test_FollowJointTrajectoryAction.cpp
+++ test/test_FollowJointTrajectoryAction.cpp
@@ -13,7 +13,7 @@ typedef actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "test_grasp");
-    Client client("/amigo/left_arm/joint_trajectory_action", true); // true -> don't need ros::spin()
+    Client client("/amigo/joint_trajectory_action", true); // true -> don't need ros::spin()
     client.waitForServer();
     control_msgs::FollowJointTrajectoryGoal goal;
 
