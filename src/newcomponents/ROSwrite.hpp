954c55becb8126641143713a79ff5eb35690e9ac
Max Baeten <m.j.j.baeten@student.tue.nl>
2016-04-10 10:04:46 +0200

    emptied component

--- src/newcomponents/ROSwrite.hpp
+++ src/newcomponents/ROSwrite.hpp
@@ -53,79 +53,13 @@ namespace ROSWRITE
 		long double aquisition_time;
 		long double start_time;
 		
-		virtual void ReadInputs();
 		virtual void CheckAllConnections();
-		virtual void MapInputs2Outputs();
-		virtual void Calculate_A();
-		virtual void Calculate_D();
+		virtual void ReadInputs();
 		virtual void WriteOutputs();
-		virtual void AddAnalogOuts(string PARTNAME, doubles INPORT_DIMENSIONS, doubles OUTPORT_DIMENSIONS, doubles FROM_WHICH_INPORT, doubles FROM_WHICH_ENTRY);
-		virtual void AddDigitalOuts(string PARTNAME, doubles INPORT_DIMENSIONS, doubles OUTPORT_DIMENSIONS, doubles FROM_WHICH_INPORT, doubles FROM_WHICH_ENTRY);
+		virtual void AddAnalogWrite(string PARTNAME, doubles INPORT_DIMENSIONS, doubles OUTPORT_DIMENSIONS, doubles FROM_WHICH_INPORT, doubles FROM_WHICH_ENTRY);
+		virtual void AddDigitalWrite(string PARTNAME, doubles INPORT_DIMENSIONS, doubles OUTPORT_DIMENSIONS, doubles FROM_WHICH_INPORT, doubles FROM_WHICH_ENTRY);
 		
-		//! AnalogOuts
-		// Ports
-		InputPort<doubles> inports_A[MAX_BODYPARTS][MAX_PORTS];
-		OutputPort<soem_beckhoff_drivers::AnalogMsg> outports_A[MAX_BODYPARTS][MAX_PORTS];
-	
-		// In/Output
-		vector< doubles > input_A[MAX_PORTS];
-		vector< doubles > temp_input_A[MAX_BODYPARTS];
-		vector< soem_beckhoff_drivers::AnalogMsg > output_msgs_A[MAX_PORTS];
 		
-		// Scalars
-		uint n_addedbodyparts_A;
-		
-		// Vectors
-		string added_bodyparts_A[MAX_BODYPARTS];
-		uint n_inports_A[MAX_BODYPARTS];
-		uint n_outports_A[MAX_BODYPARTS];
-		
-		// Matrices
-		bools addition_status_A[MAX_BODYPARTS];
-		bools multiply_status_A[MAX_BODYPARTS];
-		bools matrixtransform_status_A[MAX_BODYPARTS];
-		ints inport_dimensions_A[MAX_BODYPARTS];
-		ints outport_dimensions_A[MAX_BODYPARTS];
-		ints from_which_inport_A[MAX_BODYPARTS];
-		ints from_which_entry_A[MAX_BODYPARTS];
-		
-		// 3D
-		vector< doubles > addition_values_A[MAX_BODYPARTS];
-		vector< doubles > multiply_values_A[MAX_BODYPARTS];
-		vector< vector< doubles > > matrixtransform_entries_A[MAX_BODYPARTS];
-
-		// Functions
-		virtual void AddAddition_A(string PARTNAME, int PORTNR, doubles VALUES);
-		virtual void AddMultiply_A(string PARTNAME, int PORTNR, doubles FACTOR);
-		virtual void AddMatrixTransform_A(string PARTNAME, int PORTNR, double INPUTSIZE, double OUTPUTSIZE);
-		
-		//! DigitalOuts
-		// Ports
-		InputPort<bool> inports_D[MAX_BODYPARTS][MAX_PORTS];
-		OutputPort<soem_beckhoff_drivers::DigitalMsg> outports_D[MAX_BODYPARTS][MAX_PORTS];
-
-		// In/Output
-		bool input_D[MAX_PORTS][MAX_PORTS];
-		vector< soem_beckhoff_drivers::DigitalMsg > output_msgs_D[MAX_PORTS];
-	
-		// Scalars
-		uint n_addedbodyparts_D;
-		
-		// Vectors
-		string added_bodyparts_D[MAX_BODYPARTS];
-		uint n_inports_D[MAX_BODYPARTS];
-		uint n_outports_D[MAX_BODYPARTS];
-		
-		// Matrices
-		bools forcedhigh_status_D[MAX_BODYPARTS];
-		ints inport_dimensions_D[MAX_BODYPARTS];
-		ints outport_dimensions_D[MAX_BODYPARTS];
-		ints from_which_inport_D[MAX_BODYPARTS];
-		ints from_which_entry_D[MAX_BODYPARTS];
-		
-		// Functions
-		virtual void AddForcedHigh_D(string PARTNAME, int PORTNR);
-
 		//! Component Hooks
 		virtual bool configureHook();
 		virtual bool startHook();
