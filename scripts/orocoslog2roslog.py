5fe138dd3e098afc9c74f47345f767869e418fd3
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2017-05-05 14:24:10 +0200

    Do not crash on stupid errors

--- scripts/orocoslog2roslog.py
+++ scripts/orocoslog2roslog.py
@@ -10,7 +10,7 @@ rate = rospy.Rate(10)  # 10hz
 logfile = '/home/amigo/.ros/orocos.log'
 
 # Typical line: "5.616 [ Info   ][Thread] Supervisor: RGB_light_controller: Peer can be succesfully added"
-pattern = re.compile(r'\d+\.\d\d\d\s\[\s*(?P<severety>\w+)\s*\](?P<logline>.+)')
+pattern = re.compile(r'\d+\.\d\d\d\s\[\s*(?P<severity>\w+)\s*\](?P<logline>.+)')
 
 # Find the ending of the current log file (no use in repeating yesterdays session)
 with open(logfile) as f:
@@ -28,22 +28,22 @@ while not rospy.is_shutdown():
         for line in f:
             if not line.strip(): # Skip empty lines
                 result = pattern.search(line.decode('utf-8'))
-                severety = result.group('severety').lower()
-                logline = result.group('logline')
-                if result:
-                    if severety == "debug":
+                if result is not None:
+                    severity = result.group('severity').lower()
+                    logline = result.group('logline')
+                    if severity == "debug":
                         rospy.logdebug(logline)
-                    elif severety == "info":
+                    elif severity == "info":
                         rospy.loginfo(logline)
-                    elif severety == "warning":
+                    elif severity == "warning":
                         rospy.logwarn(logline)
-                    elif severety == "error":
+                    elif severity == "error":
                         rospy.logerr(logline)
-                    elif severety == "fatal":
+                    elif severity == "fatal":
                         rospy.logfatal(logline)
                     else:
-                        rospy.logerr('Unrecognized severety: ' + severety)
+                        rospy.logerr('Unrecognized severity: ' + severity)
                 else:
-                    rospy.logerr("No match found in orocos log")
+                    rospy.logerr("Unparsable error from orocos: %s",line)
         cur = f.tell()
     rate.sleep()
